6ae0ee2eee2d675065c100a8e457976fe722aa29
Ryan K Burns <ryankburns@gmail.com>
2020-10-13 16:38:20 +0000

    Automatically backed up by Learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,6 +1,6 @@
 class Teacher < User
 
-  KNOWLEDGE = [nil, "a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
+  KNOWLEDGE = [nil], ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 
   def teach
     puts KNOWLEDGE.sample(1)
